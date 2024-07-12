c2fac6d1f6de7276bc8fe19e880d8b2bf2b7fe3c
LemmyFl <123549154+LemmyFl@users.noreply.github.com>
2024-07-12 15:37:13 +0200

    Update AutoUpdate.sh

--- AutoUpdate.sh
+++ AutoUpdate.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-# Define the cron job and the comment
-COMMENT="# Perform system maintenance tasks at 6 AM every Sunday, with a random delay up to 59 minutes
-CRON_JOB="0 6 * * 0 sleep \$((RANDOM % 3600)) && apt-get update && apt-get -y dist-upgrade && apt-get -y autoremove && apt-get autoclean && sleep 300 && reboot > /dev/null 2>&1"
+# Define the comment and the cron job
+COMMENT="# Perform system maintenance tasks at 6 AM every Sunday, with a random delay up to 59 minutes"
+CRON_JOB='0 6 * * 0 sleep $((RANDOM % 3600)) && apt-get update && apt-get -y dist-upgrade && apt-get -y autoremove && apt-get autoclean && sleep 300 && reboot > /dev/null 2>&1'
 
 # Check if the comment and cron job already exist
 CRONTAB=$(crontab -l 2>/dev/null)
